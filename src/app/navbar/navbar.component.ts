f0c852242ae145613dcf8c72e51c23039eb80527
Abdelrahman Farghaly <abdelrhmanfarghaly1998@gmail.com>
2023-05-27 20:17:58 +0200

    Fix build error-2

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,7 +9,7 @@ import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'] 
 })
 export class NavbarComponent implements OnInit,DoCheck {
 
